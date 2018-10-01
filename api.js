d676a88c0f091a8dc2cab8a9aefcfe83d2344b74
9Slagger <b5813568@g.sut.ac.th>
2018-10-01 08:58:41 +0700

    fix api post /customer/register

--- api.js
+++ api.js
@@ -708,7 +708,7 @@ app.put('/room_usage', verifyToken, (req, res) => {
 
 app.get('/customer', (req, res) => {
   if (req.query.id) {
-    Queue.findOne({personalid:req.query.personalid}).exec(function (err, data) {
+    Customer.findOne({personalid:req.query.personalid}).exec(function (err, data) {
       if (err) {
         res.set({ 'status': '404' });
         res.status(404).json("Not Found Customer")
